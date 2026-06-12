7e789df7992fe4b475f78fbcfa5c730dac9a5092
nyashags <nyashagrecu@gmail.com>
2026-06-12 12:00:05 +0200

    Update {{ cookiecutter.project_slug }}/opt/addons/{{ 'addons_library' if true else '' }}/EnergyLimitationAbsolute.jl
    Co-authored-by: Viktoria Carmen Illyés <87526477+Villyes@users.noreply.github.com>

--- {{ cookiecutter.project_slug }}/opt/addons/{{ 'addons_library' if true else '' }}/EnergyLimitationAbsolute.jl
+++ {{ cookiecutter.project_slug }}/opt/addons/{{ 'addons_library' if true else '' }}/EnergyLimitationAbsolute.jl
@@ -7,7 +7,7 @@ module EnergyLimitationAbsolute
 #       addons: {EnergyLimitationAbsolute: {max_value: 10000, profile_name: "buy_biomethane"}}
 
 # Comments:
-# - This add on is applied to the example of limitting the output of a create profile called 'buy biomethane' which 
+# - This addon is applied to the example of limiting the aggregated output of a create-profile called 'buy biomethane' which 
 #   represents the biomethane market to 10000 energy units (i.e. MWh), but can be addapted to other applications.
 # - Note this can also be achieved by creating a stateful node rather than a create profile with an initial state 
 #   equal to the maximum output limit
